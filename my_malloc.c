4b68d13d77c3ad2e8ba1e1c6d3e1587f83663f4b
Mason Chyu <mchyu@emory.edu>
2026-02-23 18:17:36 -0500

    Initial commit for Lab 1

---
+++ my_malloc.c
@@ -0,0 +1,24 @@
+//
+//  my_malloc.c
+//  Lab1: Malloc
+//
+
+#include "my_malloc.h"
+
+MyErrorNo my_errno=MYNOERROR;
+
+void *my_malloc(uint32_t size)
+{
+}
+      
+void my_free(void *ptr)
+{
+}
+
+FreeListNode free_list_begin()
+{
+}
+
+void coalesce_free_list()
+{
+}
